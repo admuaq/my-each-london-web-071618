6a2a2d76505734ddfecb6ff5d58807c91fcc595d
Adham Muhammad <admuaq@gmail.com>
2018-07-10 16:57:25 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,11 @@
-def my_each # put argument(s) here
+def my_each(words)# put argument(s) here
   # code here
+  i = 1 
+  while i < words.length
+      yield 
+        do |n|
+        "#{n[i}"
+        i += 1
+        end 
+  end
 end
